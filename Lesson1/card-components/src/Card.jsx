4737051d8b7cd5d81909859462a35e7336d811de
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-16 21:51:10 +0800

    Added a Card.jsx

---
+++ Lesson1/card-components/src/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card() {
+
+  return(
+    <div Classname = "card">
+        <img></img>
+        <h2>Joebeck Andrew</h2>
+        <p>I am a Web Developer and a Gamer</p>
+    </div>
+  );
+
+}
+
+export default Card;
