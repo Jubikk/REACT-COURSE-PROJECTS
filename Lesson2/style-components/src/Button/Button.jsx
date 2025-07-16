3acc705bfe5686f0198e257eb3dab55ddbbe6727
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-16 23:49:00 +0800

    updated class name

--- Lesson2/style-components/src/Button/Button.jsx
+++ Lesson2/style-components/src/Button/Button.jsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 function Button (){
 
   return (
-    <button className = "button">Click me!</button>
+    <button className = {styles.button}>Click me!</button>
 
   );
 
