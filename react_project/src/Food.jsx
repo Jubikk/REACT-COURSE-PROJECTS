67e098455e9909a8ebb631fc1e4d57a10cd8021c
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-16 21:20:48 +0800

    Added Food

---
+++ react_project/src/Food.jsx
@@ -0,0 +1,17 @@
+
+function Food () {
+
+  const food1 = 'Apple';
+  const food2 = 'Banana';
+
+
+  return (
+    <ul>
+      <li>{food1.toUpperCase()}</li>
+      <li>{food2.toUpperCase()}</li>  
+    </ul>
+  );
+
+}
+
+export default Food;
