74d929b05d4cb840de84004d3701da99c3f1b7fb
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-16 23:31:36 +0800

    added css for title and card text

--- Lesson1/card-components/src/Card.jsx
+++ Lesson1/card-components/src/Card.jsx
@@ -3,9 +3,9 @@ function Card() {
 
   return(
     <div className = "card">
-        <img src= "https://scontent.fmnl17-7.fna.fbcdn.net/v/t39.30808-6/495548396_1331721751456819_1987914577470017830_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeER1uqK3LuysotTY-2bZitOhQ6aqGKsfQ2FDpqoYqx9DXc1IJZqFbTaHj_SOxVGwFV1vwxdP5ppUJHqMXazdQNY&_nc_ohc=j0_54rCi-ywQ7kNvwGArR-C&_nc_oc=Adn9DTZ33etwScqVPZT8hZ9B2McFTj1sg_v61nPxpkgHqiar7F4qsEt64AuWndG6FDI&_nc_zt=23&_nc_ht=scontent.fmnl17-7.fna&_nc_gid=Hd6n45jsva51ZFToVz9XBg&oh=00_AfQfW-4Vv3rSieFJJRFFJsJUOantvQzhmSohkvFXdCiJYw&oe=687D91C9" alt="Img not found!"></img>
-        <h2>Joebeck Andrew Gusi</h2>
-        <p>I am a Web Developer and a Gamer</p>
+        <img className= "card_image"src= "https://scontent.fmnl17-7.fna.fbcdn.net/v/t39.30808-6/495548396_1331721751456819_1987914577470017830_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeER1uqK3LuysotTY-2bZitOhQ6aqGKsfQ2FDpqoYqx9DXc1IJZqFbTaHj_SOxVGwFV1vwxdP5ppUJHqMXazdQNY&_nc_ohc=j0_54rCi-ywQ7kNvwGArR-C&_nc_oc=Adn9DTZ33etwScqVPZT8hZ9B2McFTj1sg_v61nPxpkgHqiar7F4qsEt64AuWndG6FDI&_nc_zt=23&_nc_ht=scontent.fmnl17-7.fna&_nc_gid=Hd6n45jsva51ZFToVz9XBg&oh=00_AfQfW-4Vv3rSieFJJRFFJsJUOantvQzhmSohkvFXdCiJYw&oe=687D91C9" alt="Img not found!"></img>
+        <h2 className= "card_title" >Joebeck Andrew Gusi</h2>
+        <p className= "card_text" >I am a Web Developer and a Gamer</p>
     </div>
   );
 
