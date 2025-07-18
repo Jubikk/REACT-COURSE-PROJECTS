7f6412e1d83eec2103e9e1c9435d351aa73640b4
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-19 03:06:01 +0800

    another example

--- Lesson3/props/src/App.jsx
+++ Lesson3/props/src/App.jsx
@@ -3,7 +3,10 @@ import Student from './Student.jsx';
 
 function App() {
   return (
+    <>
     <Student name = "Spongebob" age = {25} isStudent = {true}/>
+    <Student name = "Patrick" age = {22} isStudent = {false}/>
+    </>
   );
 }
 
