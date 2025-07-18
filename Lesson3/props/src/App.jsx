3b9660a145d85c229655f8de75edaa9e80ebff09
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-19 03:06:57 +0800

    examples

--- Lesson3/props/src/App.jsx
+++ Lesson3/props/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
     <>
     <Student name = "Spongebob" age = {25} isStudent = {true}/>
     <Student name = "Patrick" age = {22} isStudent = {false}/>
+    <Student name = "Squidward" age = {30} isStudent = {false}/>
+    <Student name = "Sandy" age = {24} isStudent = {true}/>
     </>
   );
 }
