fd2ee62760ae0fbfe412f3423c84165acc9bdfa2
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 04:16:57 +0800

    used javascript to sort and other things

--- Lesson5/render-lists/src/List.jsx
+++ Lesson5/render-lists/src/List.jsx
@@ -1,12 +1,24 @@
 
 function List() {
 
-  const fruits = ["banana", "apple", "orange", "kiwi", "mango"];
+  const fruits = [{id:1, name:"banana", calories: 60}, 
+                  {id:2, name:"apple", calories: 75}, 
+                  {id:3, name:"orange", calories: 54}, 
+                  {id:4, name:"kiwi", calories: 105}, 
+                  {id:5, name:"mango", calories: 150}];
 
-  const listItems = fruits.map(fruit => <li> {fruit}</li>);
+  //fruits.sort((a, b) => a.name.localeCompare(b.name));// sort by name
+  //fruits.sort((a, b) => a.calories - b.calories);// sort by calories
+
+  const listItems = fruits.map(fruit => <li key={fruit.id}> 
+                                                {fruit.name} : &nbsp; 
+                                                <b>{fruit.calories}</b></li>);
 
   return (<ol>{listItems}</ol>);
 
+
 }
+
+
 
 export default List
