e1ae324db0487320ea4f4d6f394e0fddc64c6831
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 04:32:38 +0800

    done with listing fruits

--- Lesson5/render-lists/src/App.jsx
+++ Lesson5/render-lists/src/App.jsx
@@ -3,9 +3,19 @@ import List from './List.jsx'
 
 function App() {
 
-  return (
-  <List />
-);
+  const fruits = [{id:1, name:"banana", calories: 60}, 
+                  {id:2, name:"apple", calories: 75}, 
+                  {id:3, name:"orange", calories: 54}, 
+                  {id:4, name:"kiwi", calories: 105}, 
+                  {id:5, name:"mango", calories: 150}];
+  
+  const vegetables = [{id:1, name:"banana", calories: 60}, 
+                  {id:2, name:"apple", calories: 75}, 
+                  {id:3, name:"orange", calories: 54}, 
+                  {id:4, name:"kiwi", calories: 105}, 
+                  {id:5, name:"mango", calories: 150}];                
+
+  return(<List items= {fruits} category= "Fruits"/>);
 }
 
 export default App
--- Lesson5/render-lists/src/List.jsx
+++ Lesson5/render-lists/src/List.jsx
@@ -1,11 +1,8 @@
 
-function List() {
-
-  const fruits = [{id:1, name:"banana", calories: 60}, 
-                  {id:2, name:"apple", calories: 75}, 
-                  {id:3, name:"orange", calories: 54}, 
-                  {id:4, name:"kiwi", calories: 105}, 
-                  {id:5, name:"mango", calories: 150}];
+function List(props) {
+  
+  const category = props.category;
+  const itemList = props.items;
 
   //fruits.sort((a, b) => a.name.localeCompare(b.name));// sort by name
   //fruits.sort((a, b) => a.calories - b.calories);// sort by calories
@@ -13,12 +10,13 @@ function List() {
   //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);// filter fruits with less than 100 calories
   //const highCalFruits = fruits.filter(fruit => fruit.calories > 100);// filter fruits with more than 100 calories
 
-  const listItems = highCalFruits.map(highCalFruits => <li key={highCalFruits.id}> 
-                                                                   {highCalFruits.name} : &nbsp; 
-                                                                <b>{highCalFruits.calories}</b></li>);
+  const listItems = itemList.map(item => <li key={item.id}> 
+                                                 {item.name} : &nbsp; 
+                                              <b>{item.calories}</b></li>);
+                                                
 
 
-  return (<ol>{listItems}</ol>);
+  return (<><h3>{category}</h3><ol>{listItems}</ol> </>);
 
 
 }
