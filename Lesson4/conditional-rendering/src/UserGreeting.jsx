f060eca27cfa9d4a01ae3ce220bfb96e398d4844
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 03:17:10 +0800

    used const instead of ternary

--- Lesson4/conditional-rendering/src/UserGreeting.jsx
+++ Lesson4/conditional-rendering/src/UserGreeting.jsx
@@ -1,9 +1,11 @@
 
 function UserGreeting(props) {
 
-  return (props.isLoggedIn ? <h2 className = "welcome-message">Welcome back! {props.username}</h2> : 
-                              <h2 className = "sign-in-message">Please sign in</h2>);
+  const WelcomeMessage = <h2 className="welcome-message">Welcome back! {props.username}</h2>;
+  const SignInMessage = <h2 className="sign-in-message">Please sign in</h2>;
 
+  return (props.isLoggedIn ? WelcomeMessage : SignInMessage);
+  
 }
 
 export default UserGreeting
