a238f445f64727884dafbf5fe4407885250fb234
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 04:36:35 +0800

    flash the vegetables in the page

--- Lesson5/render-lists/src/App.jsx
+++ Lesson5/render-lists/src/App.jsx
@@ -9,13 +9,19 @@ function App() {
                   {id:4, name:"kiwi", calories: 105}, 
                   {id:5, name:"mango", calories: 150}];
   
-  const vegetables = [{id:1, name:"banana", calories: 60}, 
-                  {id:2, name:"apple", calories: 75}, 
-                  {id:3, name:"orange", calories: 54}, 
-                  {id:4, name:"kiwi", calories: 105}, 
-                  {id:5, name:"mango", calories: 150}];                
+  const vegetables = [{id:1, name:"brocolli", calories: 40}, 
+                  {id:2, name:"tomatoes", calories: 85}, 
+                  {id:3, name:"pechay", calories: 33}, 
+                  {id:4, name:"squash", calories: 110}, 
+                  {id:5, name:"peas", calories: 15}];                
 
-  return(<List items= {fruits} category= "Fruits"/>);
+  return(
+    <>
+    <List items= {fruits} category= "Fruits"/>
+    <List items= {vegetables} category= "Vegetables"/>
+    </>
+    );
+
 }
 
 export default App
