a22ad18974e8fc044667e1db3beb10960c57ff35
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 18:03:34 +0800

    boolean

--- Lesson7/react-hook/src/MyComponent.jsx
+++ Lesson7/react-hook/src/MyComponent.jsx
@@ -5,6 +5,7 @@ function MyComponent() {
 
   const [name, setName] = useState("Guest");
   const [age, setAge] = useState(0);
+  const [isEmployed, setIsEmployed] = useState(false);
 
   const updateName = () => {
     setName("Spongebob");
@@ -17,6 +18,10 @@ function MyComponent() {
     setAge(age - 1);
   }
 
+  const setEmployed = () => {
+    setIsEmployed(isEmployed => !isEmployed);
+  }
+
   return(
     <div>
       <p> Name: {name}</p>
@@ -25,6 +30,9 @@ function MyComponent() {
       <p> Age: {age}</p>
       <button onClick= {IncrementAge}>Increment Age</button>
       <button onClick= {DecrementAge}>Decrement Age</button>
+
+      <p> is Employed: {isEmployed ? "Yes" : "No"}</p>
+      <button onClick= {setEmployed}>Set Employment Status</button>
     </div>
   );
 
