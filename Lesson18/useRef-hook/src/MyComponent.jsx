5a67462df3f2b9b5d00a52f588bad5a54d2f5b2a
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 07:50:45 +0800

    tried to use useRef

--- Lesson18/useRef-hook/src/MyComponent.jsx
+++ Lesson18/useRef-hook/src/MyComponent.jsx
@@ -2,20 +2,44 @@ import React, {useState, useEffect, useRef} from 'react'
 
 function MyComponent () {
   
-  const ref = useRef(0);
-
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+  
   useEffect(() => {
       console.log("COMPONENT RENDERED");
   
   });
 
-  function handleClick() {
-    ref.current++;
-    console.log(ref.current);
-
+  function handleClick1() {
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = "yellow";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "";
   }
+  function handleClick2() {
+    inputRef2.current.focus();
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "yellow";
+    inputRef3.current.style.backgroundColor = "";
+  }
+  function handleClick3() {
+    inputRef3.current.focus();
+    inputRef1.current.style.backgroundColor = "";
+    inputRef2.current.style.backgroundColor = "";
+    inputRef3.current.style.backgroundColor = "yellow";
+  }
+  
+  return (<div>
+    <button onClick={handleClick1}>Click me!</button> 
+    <input ref= {inputRef1}/>
 
-  return (<button onClick={handleClick}>Click me!</button>
+    <button onClick={handleClick2}>Click me too!</button> 
+    <input ref= {inputRef2}/>
+
+    <button onClick={handleClick3}>Click me, also!</button> 
+    <input ref= {inputRef3}/>
+  </div>
   );
 }
 
