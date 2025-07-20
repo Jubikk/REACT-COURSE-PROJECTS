7692dd45fd4b9e3c7212f689b647de670f0ba39b
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 04:10:59 +0800

    added event listener

--- Lesson15/useEffect-hook/src/MyComponents.jsx
+++ Lesson15/useEffect-hook/src/MyComponents.jsx
@@ -2,11 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 function MyComponent() {
 
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
-  return(
+  window.addEventListener("resize",handleResize);
+  console.log("EVENT LISTENER ADDED"); 
 
-    <></>
-  );
+  function handleResize () {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+
+  }
+
+  return(<>
+
+    <p>Window width: {width}px</p>
+    <p>Window height: {height}px</p>  
+
+</>);
 }
 
 export default MyComponent
