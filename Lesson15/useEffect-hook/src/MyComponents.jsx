2ff1f57c951914052e0f7af245c7a84595714ec2
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 04:06:02 +0800

    removed for another example

--- Lesson15/useEffect-hook/src/MyComponents.jsx
+++ Lesson15/useEffect-hook/src/MyComponents.jsx
@@ -2,46 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 function MyComponent() {
 
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("Red");
 
-  useEffect(() => {
-    document.title = `Count: ${count} ${color}`;
-  }, [count, color]);
+  return(
 
-  
-  function addCount () {
-    setCount(prevCount => prevCount + 1);
-  }
-
-  function subtractCount () {
-    setCount(prevCount => prevCount - 1);
-  }
-
-  function resetCount () {
-    setCount(0);
-  }
-
-  function changeColor () {
-    setColor(prevColor => prevColor === "Red" ? "Green" : "Red");
-  }
-
-
-
-
-  return (<> 
-    <p style = {{color: color}}>Count: {count}</p>
-    <button onClick={addCount}>Add</button>
-    <button onClick={subtractCount}>Subtract</button> 
-    <button onClick={resetCount}>Reset</button> <br/>
-    <button onClick={changeColor}> Change Color</button>
-    
-    
-    
-
-  </>
-);
-
+    <></>
+  );
 }
 
 export default MyComponent
