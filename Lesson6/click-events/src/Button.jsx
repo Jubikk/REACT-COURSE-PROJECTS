8e00cb2f4748f4745df0cfe3eeef3208e0490815
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 16:12:08 +0800

    used event

--- Lesson6/click-events/src/Button.jsx
+++ Lesson6/click-events/src/Button.jsx
@@ -1,21 +1,12 @@
 
 function Button () {
   
-  let count = 0;
-
-  const handleClick = (name) => {
-    if(count <3) {
-      count++;
-      console.log(`${name} you clicked me ${count} times`);
-    }
-    else {
-      console.log(`${name} Stop Clicking me!`);
-    } 
-  };
+  const handleClick = (e) => e.target.textContent = 'Clicked!';
+  
 
 
-  return (<button onClick={() => handleClick("B1Kjuu")}> Click me!</button>);
+  return (<button onClick= {(e) => handleClick(e)}> Click me!</button>);
 
-}
+};
 
 export default Button
