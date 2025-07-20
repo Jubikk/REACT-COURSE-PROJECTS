0a1a11bb35ce936377c79bc1bea0b59d9cab68e9
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 01:41:47 +0800

    added css to make it look better

--- Lesson13/update-objects-array/src/MyComponents.jsx
+++ Lesson13/update-objects-array/src/MyComponents.jsx
@@ -45,14 +45,14 @@ function MyComponent() {
   <div>
     <h2> List of Car Objects</h2>
     <ul>
-      {cars.map((car, index) => <li key ={index} onClick = {() => handleRemoveCar(index)}> {car.year} {car.make} {car.model} </li>)}
+      {cars.map((car, index) => <li className= "remove-car" key ={index} onClick = {() => handleRemoveCar(index)}> {car.year} {car.make} {car.model} </li>)}
     </ul>
-
+    <div className= "input-field">
     <input type= "number" value = {carYear} onChange={handleYearChange}/> <br/>
     <input type= "text" value = {carMake} onChange={handleMakeChange} placeholder= " Enter Car Make"/> <br/>
     <input type= "string" value = {carModel} onChange={handleModelChange} placeholder= " Enter Car Model"/> <br/>
     <button onClick = {handleAddCar} >Add Car</button>
-
+    </div>
   </div>
   
   
