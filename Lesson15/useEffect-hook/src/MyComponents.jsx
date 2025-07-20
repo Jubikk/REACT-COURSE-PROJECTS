1e5403c89a60ef799224b29747e4ac61c42bd1d5
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 04:00:27 +0800

    added functions and display

--- Lesson15/useEffect-hook/src/MyComponents.jsx
+++ Lesson15/useEffect-hook/src/MyComponents.jsx
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function MyComponent() {
 
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("Red");
+
+  useEffect(() => {
+    document.title = `Count: ${count} ${color}`;
+  }, [count]);
+
+  
+  function addCount () {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  function subtractCount () {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function changeColor () {
+    setColor(prevColor => prevColor === "Red" ? "Green" : "Red");
+  }
+
+
   return (<> 
- 
+    <p style = {{color: color}}>Count: {count}</p>
+    <button onClick={addCount}>Add</button>
+    <button onClick={subtractCount}>Subtract</button> <br/>
+    <button onClick={changeColor}> Change Color</button>
+    
+    
+
   </>
 );
 
