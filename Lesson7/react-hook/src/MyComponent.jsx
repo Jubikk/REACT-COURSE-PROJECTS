0b2485a997ea5f53ce17121ca24bdde638e50f57
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-20 17:56:34 +0800

    Increment method for age

--- Lesson7/react-hook/src/MyComponent.jsx
+++ Lesson7/react-hook/src/MyComponent.jsx
@@ -4,15 +4,23 @@ import React, {useState} from 'react';
 function MyComponent() {
 
   const [name, setName] = useState("Guest");
+  const [age, setAge] = useState(0);
 
   const updateName = () => {
     setName("Spongebob");
+  }
+
+  const IncrementAge = () => {
+    setAge(age + 1);
   }
 
   return(
     <div>
       <p> Name: {name}</p>
       <button onClick= {updateName}>Set name</button>
+
+      <p> Age: {age}</p>
+      <button onClick= {IncrementAge}>Increment Age</button>
     </div>
   );
 
