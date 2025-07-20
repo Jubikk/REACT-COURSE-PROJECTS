5fadf544dca9192ab7812305c6ec5475920ffebc
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 07:01:31 +0800

    using the usual useEffect

--- Lesson18/useRef-hook/src/MyComponent.jsx
+++ Lesson18/useRef-hook/src/MyComponent.jsx
@@ -1,7 +1,20 @@
 import React, {useState, useEffect, useRef} from 'react'
 
 function MyComponent () {
+  
+  let [number, setNumber] = useState(0);
 
+  useEffect(() => {
+      console.log("COMPONENT RENDERED");
+  
+  });
+
+  function handleClick() {
+    setNumber(prevnum => prevnum + 1);
+  }
+
+  return (<button onClick={handleClick}>Click me!</button>
+  );
 }
 
 export default MyComponent
