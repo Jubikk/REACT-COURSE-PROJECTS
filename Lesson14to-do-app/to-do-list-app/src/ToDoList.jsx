e638298219158361461554dded14d730be02d74d
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 02:39:10 +0800

    added functions and display

--- Lesson14to-do-app/to-do-list-app/src/ToDoList.jsx
+++ Lesson14to-do-app/to-do-list-app/src/ToDoList.jsx
@@ -2,11 +2,43 @@ import React, { useState } from "react";
 
 
 function ToDoList (){
+  const [task, setTask] = useState(["Eat breakfast", "Take a bath", "Learn more about React"]);
+  const [newTask, setNewTask] = useState("");
 
 
-  return (
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+  }
 
-    <></>
+  function addTask () {
+
+  }
+
+  function deleteTask (index) {
+
+  }
+
+  function moveTaskUp (index) {
+
+  }
+
+  function moveTaskDown (index) {
+
+  }
+
+
+  return ( <div className = "to-do-list"> 
+    <h1>To-Do-List</h1>
+
+    <div>
+      <input type ="text" 
+             placeholder = "Enter new task..." 
+             value = {newTask} 
+             onChange = {handleInputChange}/>
+      <button className = "add-button" onClick= {addTask}>Add Task</button>
+    </div>
+  
+  </div>
   );
 
 }
