ded01575ab470f0274db5c208607167152d4cc38
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 06:31:57 +0800

    duplicated the file and renamed it to ComponentB,C and D

---
+++ Lesson17/useContext-hook/src/ComponentB.jsx
@@ -0,0 +1,13 @@
+
+
+function ComponentB () {
+
+
+  return (
+    <div className="box">
+      <h1>Component B</h1>
+    </div>
+  );
+}
+
+export default ComponentB
---
+++ Lesson17/useContext-hook/src/ComponentC.jsx
@@ -0,0 +1,13 @@
+
+
+function ComponentC () {
+
+
+  return (
+    <div className="box">
+      <h1>Component C</h1>
+    </div>
+  );
+}
+
+export default ComponentC
