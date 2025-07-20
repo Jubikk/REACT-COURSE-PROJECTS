d8440e64e25b376b168d571049757a306051f7a5
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 04:42:35 +0800

    - added the digitalclock to return statement - added necessary display

--- Lesson16digital-clock-app/digital-clock-app/src/DigitalClock.jsx
+++ Lesson16digital-clock-app/digital-clock-app/src/DigitalClock.jsx
@@ -3,12 +3,14 @@ import React, {useState, useEffect} from "react";
 function DigitalClock(){
 
 
-  return (<>
+  return (<div className="clock-container">
+    <div className="clock"> 
+      <span>00:00:00</span>
+    </div>
   
   
-  
-  
-  </>
+
+  </div>
 );
 
 
