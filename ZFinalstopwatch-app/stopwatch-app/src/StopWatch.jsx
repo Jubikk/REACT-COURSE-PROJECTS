230fd91a209342300c35140a1c8eef24657f4d9b
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-21 09:30:44 +0800

    added display

--- ZFinalstopwatch-app/stopwatch-app/src/StopWatch.jsx
+++ ZFinalstopwatch-app/stopwatch-app/src/StopWatch.jsx
@@ -8,8 +8,36 @@ function StopWatch () {
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef (0);
 
+  useEffect(() => {
+
+  },[isRunning]);
+
+  function start(){
+
+
+  }
+
+  function stop (){
+
+  }
+
+  function reset () {
+
+  }
+
+  function formatTime(){
+  
+    return "00:00:00";
+  }
   return(
-    <></>
+    <div className="stopwatch">
+      <div className="display">{formatTime()}</div>
+      <div className="controls">
+        <button onClick={start} className="start-button">Start</button>
+        <button onClick={stop} className="stop-button">Stop</button>
+        <button onClick={reset} className="reset-button">Reset</button>
+      </div>
+    </div>
   );
 }
 
